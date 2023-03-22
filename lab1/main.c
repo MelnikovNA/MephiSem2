88b39bf68fafdf3ce3185e102461e05e3979e9d4
stif1550 <nibidocall@gmail.com>
2023-03-22 19:28:14 +0300

    4

--- lab1/main.c
+++ lab1/main.c
@@ -8,6 +8,9 @@ int main() {
             &minus_int, &mult_int,  &new_int,
             &str_to_val_int, &val_to_str_int);
     struct vectors *vector = from_file("C:\\Cprog\\MephiSem2\\lab1\\test", ri);
-    print_v(ri, vector);
+    //struct Vector3* res = sum_vec(vector->v3s[1], vector->v3s[2], ri);
+   //struct Vector3* res = mult(vector->v3s[1], vector->v3s[2], ri); работает не правильно
+   //void * res = mult_sc(vector->v3s[1], vector->v3s[2], ri);
+    print_v(vector);
     return 0;
 }
